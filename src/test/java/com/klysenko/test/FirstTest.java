2f33af2d08aaf60e3f1d167dbafa0d140dfab499
Kateryna Lysenko <leontieva.kateryna@gmail.com>
2020-06-11 14:49:44 +0300

    added configuration for running tests on  browserstack

--- src/test/java/com/klysenko/test/FirstTest.java
+++ src/test/java/com/klysenko/test/FirstTest.java
@@ -1,6 +1,5 @@
 package com.klysenko.test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,16 +7,30 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class FirstTest {
 
     WebDriver driver;
 
     @BeforeEach
-    void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    void setUp() throws MalformedURLException {
+        String USERNAME = "bsuser70982";
+        String AUTOMATE_KEY = "UcVgFzxkVw1m3TZZpGyx";
+        String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        caps.setCapability("os", "OS X");
+        caps.setCapability("os_version", "Catalina");
+        caps.setCapability("browser", "Safari");
+        caps.setCapability("browser_version", "13");
+        caps.setCapability("name", "bsuser70982's First Test");
+        driver = new RemoteWebDriver(new URL(URL), caps);
     }
 
     @AfterEach
@@ -28,8 +41,8 @@ public class FirstTest {
     @Test
     void simpleTest() {
         driver.get("http://google.com");
-        driver.findElement(By.name("q")).sendKeys("Selenium"+ Keys.ENTER);
-        Assertions.assertEquals("Selenium",driver.findElement(By.tagName("h3")).getText());
+        driver.findElement(By.name("q")).sendKeys("Selenium" + Keys.ENTER);
+        Assertions.assertEquals("Selenium", driver.findElement(By.tagName("h3")).getText());
     }
 
     @Test
